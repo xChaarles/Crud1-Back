74b6d425309c797aba6f671d4932edd0c10d2aa1
xchaarles <carlosbp2608@gmail.com>
2024-09-08 21:40:30 -0500

    Creacion de exception de archivo de errordetails para una repsuesta mas personalizada del NOT_FOUND

--- src/main/java/com/carlos/crud_fullstack_angular/exception/ResourceNotFoundException.java
+++ src/main/java/com/carlos/crud_fullstack_angular/exception/ResourceNotFoundException.java
@@ -3,6 +3,11 @@ package com.carlos.crud_fullstack_angular.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class ResourceNotFoundException extends RuntimeException {
+
+    private String message;
+    public ResourceNotFoundException(String message){
+        super(message);
+        this.message = message;
+    }
 }
